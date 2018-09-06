867d3d8dda43d67c4345935f23fc29ba716e0c1a
Lucas North <contact@lucasnorth.uk>
2018-09-06 14:58:57 +0100

    Update after.php

--- after.php
+++ after.php
@@ -2,7 +2,7 @@
 
 
 <a href="<?php echo $electionDataResult[4] ?>">Privacy</a><br>
-<a href="http://lucasnorth.uk/#/portfolio/LIES.html"><h5>Powered by LIES</h5></a>
+<a href="https://lucasnorth.uk/#/portfolio/LIES.html"><h5>Powered by LIES</h5></a>
 
 
 </div></html>
